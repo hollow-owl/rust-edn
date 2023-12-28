457f545eaa8373c52084a2b4d842aa62cb429912
hollow-owl <71401395+hollow-owl@users.noreply.github.com>
2023-12-28 16:34:48 -0600

    Better matching of clojure's regexs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,17 @@ use pest_derive::Parser;
 #[derive(Parser)]
 #[grammar = "edn.pest"] // relative to src
 struct EdnParser;
+// [:]?([\D&&[^/]].*/)?([\D&&[^/]][^/]*)
+// static Pattern symbolPat = Pattern.compile("[:]?([\\D&&[^/]].*/)?([\\D&&[^/]][^/]*)");
+// ([-+]?)(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)(N)?
+// static Pattern intPat =
+// 		Pattern.compile(
+// 				"([-+]?)(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)(N)?");
+// ([-+]?[0-9]+)/([0-9]+)
+// static Pattern ratioPat = Pattern.compile("([-+]?[0-9]+)/([0-9]+)");
+// ([-+]?[0-9]+(\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?
+// static Pattern floatPat = Pattern.compile("([-+]?[0-9]+(\\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?");
+// static final Symbol SLASH = Symbol.intern("/");
 
 fn main() {
     let mut file = File::open("test/learnxiny.edn").unwrap();
